535925ca870084bc465bce3ca1eee8c8a9bee5bb
Sartan322 <43168826+Sartan322@users.noreply.github.com>
2018-09-20 16:10:57 +0300

    Homework 5 is completed

--- h3.java
+++ h3.java
@@ -1,21 +1,24 @@
+package com.company;
+
 import java.util.Scanner;
 
-public class h3  {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		String str = "Digits:";
-		for(int i=1;i<n-1;i++){
-			 int c = sc.nextInt();
-			 if(b>a&&b>c){
-				 str = str + b + " ";
-				 
-			 }
-			 a=b;
-			 b=c;
-		}
-		System.out.print(str);
-	}
+public class h3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        String str = sc.next();
+        int a = 0;
+        char[] ch = str.toCharArray();
+        double[] ar = new double[str.length()];
+        for (int i = 0; i < str.length() ; i++) {
+            double x = ch[i]-48;
+            System.out.println(x);
+            ar[i]=x * Math.pow (10, str.length()-1-i);
+        }
+        for (int i = 0; i < str.length() ; i++) {
+            a+= (int) ar[i];
+        }
+        System.out.println(a);
+
+    }
 }
